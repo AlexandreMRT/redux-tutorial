25033e40107dc1ed50cca08ebf0a7485ef46c6c0
Alexandre <mrtalexandre@gmail.com>
2021-01-13 22:51:24 -0300

    Add action types to store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const initialState = {
-  amount: "10.00",
+  amount: "100.00",
   currencyCode: "USD",
-}
+};
 
 function reducer(state = initialState, action) {
-  return state
+  console.log("action.type :>> ", action.type);
+  switch (action.type) {
+    case "amountChanged":
+      return { ...state, amount: action.payload };
+    case "currencyCodeChanged":
+      console.log("action :>> ", action);
+      return { ...state, currencyCode: action.payload };
+    default:
+      return state;
+  }
 }
 
 export const store = createStore(reducer);
