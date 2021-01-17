b6bd4f140a73bc59ff032d3351c581dfa72aa89a
Alexandre <mrtalexandre@gmail.com>
2021-01-17 03:46:46 -0300

    Update app to use action types and action creators from the redux store

--- src/store/rates.js
+++ src/store/rates.js
@@ -19,3 +19,20 @@ export function ratesReducer(state = initialState, action) {
 
 export const getAmount = (state) => state.rates.amount;
 export const getCurrencyCode = (state) => state.rates.currencyCode;
+
+//action types
+
+export const AMOUNT_CHANGED = 'rates/amountChanged';
+export const CURRENCY_CODE_CHANGED = 'rates/currencyCodeChanged';
+
+//action creators
+
+export const changeAmount = (amount) => ({
+  type: AMOUNT_CHANGED,
+  payload: amount,
+});
+
+export const changeCurrencyCode = (currencyCode) => ({
+  type: CURRENCY_CODE_CHANGED,
+  payload: currencyCode,
+});
